d1770f797f680769f449ef33dd7798bdea159bef
Hedia <hedialassoued25@gmail.com>
2018-11-09 14:39:39 +0000

    commit 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BannerComponent } from './components/banner/banner.component';
+
 import { NgModule, Component } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 const appRoutes: Routes = [
-    { path: 'navbar', component: NavbarComponent},
-    { path: 'banner', component: BannerComponent},
-    { path: 'footer', component: FooterComponent},
-    { path: 'header', component: HeaderComponent},
-];
+    { path: 'navbar', component: NavbarComponent},]
+  
 @NgModule({
 imports: [RouterModule.forRoot(appRoutes)],
 exports: [RouterModule]
