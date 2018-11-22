a484dd3c4a4f36f14359d26c84c9df7fd232869c
yosrennouri <yosserennouri@gmail.com>
2018-11-22 16:48:07 +0100

    Ajout Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { OrdinateurDeBureauComponent } from './components/ordinateur/ordinateur-de-bureau/ordinateur-de-bureau.component';
+import { PCComponent } from './components/ordinateur/pc/pc.component';
+import { DisquesInternesComponent } from './components/stockage/disques-internes/disques-internes.component';
+import { DisquesExternesComponent } from './components/stockage/disques-externes/disques-externes.component';
+import { CleUSBComponent } from './components/stockage/cle-usb/cle-usb.component';
+import { CarteMemoireComponent } from './components/stockage/carte-memoire/carte-memoire.component';
+import { ScannerComponent } from './components/Impression/scanner/scanner.component';
+import { ImprimanteComponent } from './components/Impression/imprimante/imprimante.component';
+import { PhotocopieurComponent } from './components/Impression/photocopieur/photocopieur.component';
+import { ProcesseurComponent } from './components/Composants-Informatiques/processeur/processeur.component';
+import { VentilateurComponent } from './components/Composants-Informatiques/ventilateur/ventilateur.component';
+import { CarteMereComponent } from './components/Composants-Informatiques/carte-mere/carte-mere.component';
+import { CarteGraphiqueComponent } from './components/Composants-Informatiques/carte-graphique/carte-graphique.component';
+import { SourisComponent } from './components/Peripheriques-Accessoires/Péripheriques/souris/souris.component';
+import { ClavierComponent } from './components/Peripheriques-Accessoires/Péripheriques/clavier/clavier.component';
+import { WebCamComponent } from './components/Peripheriques-Accessoires/Péripheriques/web-cam/web-cam.component';
+import { CasquesComponent } from './components/Peripheriques-Accessoires/Péripheriques/casques/casques.component';
+import { ManetteComponent } from './components/Peripheriques-Accessoires/Péripheriques/manette/manette.component';
+import { SacPCComponent } from './components/Peripheriques-Accessoires/Accessoires/sac-pc/sac-pc.component';
+import { TapisComponent } from './components/Peripheriques-Accessoires/Accessoires/tapis/tapis.component';
+import { LampeComponent } from './components/Peripheriques-Accessoires/Accessoires/lampe/lampe.component';
+
 
 
 
@@ -18,7 +40,29 @@ import { ProductListComponent } from './components/product-list/product-list.com
     FooterComponent,
     NavbarComponent,
     HeaderComponent,
-    ProductListComponent
+    ProductListComponent,
+    OrdinateurDeBureauComponent,
+    PCComponent,
+    DisquesInternesComponent,
+    DisquesExternesComponent,
+    CleUSBComponent,
+    CarteMemoireComponent,
+    ScannerComponent,
+    ImprimanteComponent,
+    PhotocopieurComponent,
+    ProcesseurComponent,
+    VentilateurComponent,
+    CarteMereComponent,
+    CarteGraphiqueComponent,
+    SourisComponent,
+    ClavierComponent,
+    WebCamComponent,
+    CasquesComponent,
+    ManetteComponent,
+    SacPCComponent,
+    TapisComponent,
+    LampeComponent
+
     
 
     
